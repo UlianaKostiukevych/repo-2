22dea90efb713fcb5df0cd649a56f76e737013bd
Uliana S <uliana.kostiukevych@garantme.fr>
2023-05-14 19:39:14 +0300

    Create e2e test - Fill and submit Text box from

--- cypress/e2e/HomW-12/Visit-demoqa.cy.js
+++ cypress/e2e/HomW-12/Visit-demoqa.cy.js
@@ -1,6 +1,14 @@
 describe("Demoqa test suite", () => {
-        it("Visit site", () => {
-            cy.visit("https://demoqa.com/text-box");
-            cy.url().should("include", "/text-box");
-        });
+    beforeEach(() => {
+        cy.visit("/text-box");
+        cy.url().should("include", "/text-box");
     })
+
+    it("Fill Text Box form", () => {
+        cy.get("#userName").type("Uliana Senenka");
+        cy.get("#userEmail").type("testmail@eyepasta.com");
+        cy.get("#currentAddress").type("Backer str, 221b, London");
+        cy.get("#permanentAddress").type("Avengers Tower, New York");
+        cy.get("#submit").click();
+    })
+})
