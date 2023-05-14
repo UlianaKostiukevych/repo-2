36493bea7d3baa24d055f938f8aabe370bf8f292
Uliana S <uliana.kostiukevych@garantme.fr>
2023-05-14 22:27:06 +0300

    Add faker tests

--- cypress/e2e/HomW-12/Visit-demoqa.cy.js
+++ cypress/e2e/HomW-12/Visit-demoqa.cy.js
@@ -1,16 +1,23 @@
+import { faker } from '@faker-js/faker';
+const randomName = faker.name.fullName(); // Rowan Nikolaus
+const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+const randomCurrentAddress = faker.currentAddress.streetAddress();
+const randomPermamentAddress = faker.permanentAddress.streetAddress();
+
+
 describe("Demoqa test suite", () => {
     it("Fill Text Box form", () => {
         cy.visit("/text-box");
-        cy.get("#userName").type("Uliana Senenka");
-        cy.get("#userEmail").type("testmail@eyepasta.com");
-        cy.get("#currentAddress").type("Backer str, 221b, London");
-        cy.get("#permanentAddress").type("Avengers Tower, New York");
+        cy.get("#userName").type(randomName);
+        cy.get("#userEmail").type(randomEmail);
+        cy.get("#currentAddress").type(randomCurrentAddress);
+        cy.get("#permanentAddress").type(randomPermamentAddress);
         cy.get("#submit").click();
         cy.get("#output").should("exist");
-        cy.get("#name").should("have.text", "Name:Uliana Senenka");
-        cy.get("#email").should("have.text", "Email:testmail@eyepasta.com");
-        cy.get("p#currentAddress").should("contain.text", "Current Address :Backer str, 221b, London");
-        cy.get("p#permanentAddress").should("contain.text", "Permananet Address :Avengers Tower, New York");
+        cy.get("#name").should("have.text", randomName);
+        cy.get("#email").should("have.text", randomEmail);
+        cy.get("p#currentAddress").should("contain.text", `Current Address :${randomCurrentAddress}`);
+        cy.get("p#permanentAddress").should("contain.text", `Permananet Address :${randomPermamentAddress}`);
 
     })
 })
