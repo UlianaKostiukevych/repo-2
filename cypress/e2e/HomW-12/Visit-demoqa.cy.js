3ef4df2f9ba46fe0b9cf783d2b60e64427fb2c83
Uliana S <uliana.kostiukevych@garantme.fr>
2023-05-14 21:57:29 +0300

    Add output test

--- cypress/e2e/HomW-12/Visit-demoqa.cy.js
+++ cypress/e2e/HomW-12/Visit-demoqa.cy.js
@@ -1,14 +1,16 @@
 describe("Demoqa test suite", () => {
-    beforeEach(() => {
-        cy.visit("/text-box");
-        cy.url().should("include", "/text-box");
-    })
-
     it("Fill Text Box form", () => {
+        cy.visit("/text-box");
         cy.get("#userName").type("Uliana Senenka");
         cy.get("#userEmail").type("testmail@eyepasta.com");
         cy.get("#currentAddress").type("Backer str, 221b, London");
         cy.get("#permanentAddress").type("Avengers Tower, New York");
         cy.get("#submit").click();
+        cy.get("#output").should("exist");
+        cy.get("#name").should("have.text", "Name:Uliana Senenka");
+        cy.get("#email").should("have.text", "Email:testmail@eyepasta.com");
+        cy.get("p#currentAddress").should("contain.text", "Current Address :Backer str, 221b, London");
+        cy.get("p#permanentAddress").should("contain.text", "Permananet Address :Avengers Tower, New York");
+
     })
 })
