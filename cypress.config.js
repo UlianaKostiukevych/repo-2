6ef41feacf4d7751ec1b5bfb6cad6ec1deee893d
Uliana S <90183988+UlianaKostiukevych@users.noreply.github.com>
2023-08-13 20:47:39 +0300

    Hom w 18 (#11)
    * 1
    
    * HomW-18 - 1
    
    * HomW-18 - 2
    
    * HomW-18 - 3
    
    * Finish HomW-18

--- cypress.config.js
+++ cypress.config.js
@@ -8,12 +8,17 @@ module.exports = defineConfig({
     pageLoadTimeout: 60000,
     requestTimeout: 5000,
     responsetTimeout: 5000,
-    viewportHeight: 660, 
+    viewportHeight: 660,
     viewportWidth: 1000,
-    specPattern: "cypress/e2e/**/*.cy.{js, jsx, ts, tsx}",
-    //excludeSpecPattern: "cypress/e2e/**/*.cy.{js, jsx, ts, tsx}",
-    setupNodeEvents(on, config) {
+    specPattern: "cypress/e2e/**/*.{js, jsx, ts, tsx}",
+    excludeSpecPattern: "cypress/e2e/**/*.cy.{js, jsx, ts, tsx}",
+    trashAssetsBeforeRuns: true,
+    setupNodeEvents() {
       // implement node event listeners here
     },
   },
+  /*env: {
+    API_BASE_URL: "https://reqres.in",
+    BOOKER_API_URL: "https://restful-booker.herokuapp.com",
+  },*/
 });
